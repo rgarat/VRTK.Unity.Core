7c05682ee5aa7444aa85cdb756fc75887314d814
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-08-18 12:38:32 +0200

    feat(BodyPhysics): body physics based on CharacterController
    The Unity `CharacterController` was made for non-VR usage but is
    equally helpful when implementing the concept of "body physics" for
    VR. Three new components have been added that allow setting the
    height of the controller to match a given Game Object, moving a Game
    Object to be at the controller's top and most importantly moving the
    controller by specifying a movement from one Game Object to the
    other to replicate. With these components a body representation in
    form of the controller is able to follow and match a VR headset
    moving in the real world while adhering to the virtual world's
    constraints imposed by both the physical world as well as other
    components affecting the controller.

---
+++ Scripts/Tracking/CharacterController/CharacterControllerMover.cs
@@ -0,0 +1,53 @@
+﻿namespace VRTK.Core.Tracking.CharacterController
+{
+    using UnityEngine;
+    using VRTK.Core.Data.Source;
+    using VRTK.Core.Process;
+
+    /// <summary>
+    /// Moves a <see cref="CharacterController"/> by following the movement of two <see cref="GameObject"/>s.
+    /// </summary>
+    public class CharacterControllerMover : MonoBehaviour, IProcessable
+    {
+        /// <summary>
+        /// The controller to move.
+        /// </summary>
+        [Tooltip("The controller to move.")]
+        public CharacterController characterController;
+        /// <summary>
+        /// The source of the movement motion.
+        /// </summary>
+        [Tooltip("The source of the movement motion.")]
+        public GameObject source;
+        /// <summary>
+        /// The target of the movement motion.
+        /// </summary>
+        [Tooltip("The target of the movement motion.")]
+        public GameObject target;
+
+        /// <summary>
+        /// An optional source of gravity to apply when moving <see cref="characterController"/>.
+        /// </summary>
+        [Tooltip("An optional source of gravity to apply when moving character controller.")]
+        public Vector3SourceContainer gravitySource;
+        /// <summary>
+        /// Whether to apply <see cref="gravitySource"/> to the movement motion.
+        /// </summary>
+        [Tooltip("Whether to apply gravity source to the movement motion.")]
+        public bool applyGravity = true;
+
+        /// <summary>
+        /// Moves <see cref="characterController"/> based on the movement of <see cref="source"/> to <see cref="target"/>.
+        /// </summary>
+        public virtual void Process()
+        {
+            Vector3 motion = target.transform.position - source.transform.position;
+            if (applyGravity)
+            {
+                motion += gravitySource?.Interface?.Vector ?? Vector3.zero;
+            }
+
+            characterController.Move(motion);
+        }
+    }
+}
